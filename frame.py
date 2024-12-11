44cf523adf2f94ee3ef2407326e85e69a7c796a0
Olga-shimonis <simonisolga@gmail.com>
2024-12-11 14:18:26 +0300

    1

--- frame.py
+++ frame.py
@@ -1,8 +1,31 @@
 
 import cv2 as cv
 import numpy as np
-from registration import Model, register
+from registration_1 import Model, register
 from detection import Detector
+
+
+
+
+
+
+
+def detect_pose(p_feat, sift_3d): #height weight from model
+    if len(p_feat) > 3 and len(p_feat) == len(sift_3d):
+        valid, rvec, tvec = cv.solvePnP(sift_3d, p_feat, cameraMatrix, distCoeffs)
+        rvecs = cv.Rodrigues(rvec)[0]
+        return valid, rvecs, tvec
+        #else:
+          #  return False
+    return False
+
+def registration_transform_3d_corners(rvecs, tvec, cameraMatrix, distCoeffs): #height weight from model
+    pts = np.float32(model.object_corners_3d)
+    #pts = np.float32([[0, 0, 0], [0, h - 1, 0], [w - 1, h - 1, 0], [w - 1, 0, 0]]) #3d corners
+    t, _ = cv.projectPoints(pts, rvecs, tvec, cameraMatrix, distCoeffs)
+
+    t = t.reshape(-1, 2)
+    return t
 
 
 
@@ -21,28 +44,34 @@ class frame_by_registration:
                                    minDistance=self.min_distance,
                                    blockSize=self.block_size)
 
-    def registrate_model(self, reference, camera_params):
+    def registrate_model(self, reference, camera_params, h, w):
         model = Model()
         model.load_camera_params(camera_params)
-
         model.upload_image(reference, 'output.jpg')
         model.register('SIFT')
         model_path = 'model_params.npz'
+        model.crop_method = 'corner'
+        model.object_corners_2d = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]])
+        model.object_corners_3d = np.array([
+                                        [0, 0, 0],  # Top-left
+                                        [w-1, 0, 0],  # Top-right
+                                        [w-1, h-1, 0],  # Bottom-right
+                                        [0, h-1, 0],  # Bottom-left
+                                    ], dtype="float32")
         model.save_to_npz(model_path)
         model = Model.load(model_path)
         return model
 
 
-    def find_good_sift(self, shot, model):
-        MIN_MATCH_COUNT = 10
-        kp1, des1 = model.kp, model.des
-        detector = Detector()
-        detector.get_model_params(model)
-        detector.instance_method(True)
+    def find_good_sift(self, shot, model, h, w, img1):
 
-        detector.detect_image(shot, useFlann=True, drawMatch=False)
-        kp2, des2 = detector.kp, detector.des
-        sift_3d = detector.p_3D
+        '''dst, features, s_d = detect_features(self, shot, model, h, w, img1)
+        #s_d = registration_find_3d_sifts(ob)'''
+        MIN_MATCH_COUNT = 10
+        sift = cv.SIFT_create()
+        kp1, des1 = model.kp, model.des
+        #kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(shot, None)
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
@@ -62,16 +91,21 @@ class frame_by_registration:
             M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
             matchesMask = mask.ravel().tolist()
 
-            h, w = img1.shape
-            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-            dst = cv.perspectiveTransform(pts, M)
+            #pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            #dst = cv.perspectiveTransform(pts, M)
             features = []
 
-
+            ob = []
             for i, m in enumerate(good):
                 if matchesMask[i]:
                     features.append(dst_pts[i])
+                    ob.append(src_pts[i])
 
+            s_d = []
+            for i in ob:
+                x, y = i[0][0], i[0][1]
+                s_d.append([x, y, 0])
+            s_d = np.array(s_d)
 
             features = np.array(features)
 
@@ -79,11 +113,11 @@ class frame_by_registration:
             print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
             matchesMask = None
 
-        '''return dst, features, s_d'''
-        return sift_3d, features
+        return s_d, features
 
 
-    def track_features_sift(self, previous_frame, current_frame, feat, flag):
+
+    def track_features_sift(self, previous_frame, current_frame, feat):
         frame_gray = cv.cvtColor(current_frame, cv.COLOR_BGR2GRAY)
         old_gray = cv.cvtColor(previous_frame, cv.COLOR_BGR2GRAY)
 
@@ -92,41 +126,34 @@ class frame_by_registration:
         good_new_feat = p_feat[st_feat == 1]
         good_old_feat = feat[st_feat == 1]
 
-        if flag:
-            return good_new_feat, good_old_feat, p_feat
 
-        return good_new_feat, good_old_feat
 
-    def draw_tracks(self, mask, frame, good_new, good_old):
-        for i, (new, old) in enumerate(zip(good_new, good_old)):
-            a, b = new.ravel()
 
-            c, d = old.ravel()
 
-            cv.line(mask, (int(a), int(b)), (int(c), int(d)), (0, 255, 0), 1)
+        return good_new_feat, good_old_feat, p_feat
 
-            frame = cv.circle(frame, (int(a), int(b)), 2, (0, 0, 255), -1)
-        return mask, frame
 
-    def draw_frame(self, frame, sift_3d, p_feat, cameraMatrix, distCoeffs, model):
-        #h, w = img1.shape
-        #pts = np.float32([[0, 0, 0], [0, h - 1, 0], [w - 1, h - 1, 0], [w - 1, 0, 0]])
-        pts = np.float32(model.object_corners_2d)
+
+    def draw_frame(self, frame, sift_3d, sift_2d, cameraMatrix, distCoeffs, model, h, w, p_feat):
+        #if detect_pose(sift_2d, sift_3d) != False:
+        print(len(p_feat), len(sift_3d))
         if len(p_feat) > 3 and len(p_feat) == len(sift_3d):
             valid, rvec, tvec = cv.solvePnP(sift_3d, p_feat, cameraMatrix, distCoeffs)
             rvecs = cv.Rodrigues(rvec)[0]
-
-            t, _ = cv.projectPoints(pts, rvecs, tvec, cameraMatrix, distCoeffs)
-
-            t = t.reshape(-1, 2)
+            #valid, rvecs, tvec = detect_pose(sift_2d, sift_3d)
+            t = registration_transform_3d_corners(rvecs, tvec, cameraMatrix, distCoeffs)
             frame = cv.polylines(frame, [np.int32(t)], True, 255, 3, cv.LINE_AA)
+
         return frame
+
 
 
 # Example usage
 if __name__ == "__main__":
-    img1 = 'reference.jpg'   #load reference photo
+    img1 = cv.imread('reference.jpg', cv.IMREAD_GRAYSCALE)
+    h, w = img1.shape
 
+    img_ = 'reference.jpg'   #load reference photo
     camera_params = 'cam_params.npz'  #load camera parameters
 
     with np.load('cam_params.npz') as file:
@@ -137,17 +164,17 @@ if __name__ == "__main__":
     cap = cv.VideoCapture('WIN_20241112_20_14_06_Pro.mp4')  #load video file
 
     tracker = frame_by_registration()
-    model = tracker.registrate_model(img1, camera_params)
+    model = tracker.registrate_model(img_, camera_params, h, w)
 
     ret, previous_frame = cap.read()
     if not ret:
         print("Failed to read the first frame.")
         exit()
 
-    pts_3d, feat = tracker.find_good_sift(previous_frame, model)
+    pts_3d, feat = tracker.find_good_sift(previous_frame, model, h, w, img1)
     mask = np.zeros_like(previous_frame)
     Images = []
-    n = 100
+    n = 50
     count = 1
     while True:
         ret, frame = cap.read()
@@ -156,21 +183,21 @@ if __name__ == "__main__":
 
         if count % n == 0:
             ret, previous_frame = cap.read()
-            pts_3d, feat = tracker.find_good_sift(previous_frame, model)
+            pts_3d, feat = tracker.find_good_sift(previous_frame, model, h, w, img1)
             mask = np.zeros_like(previous_frame)
 
 
-        good_new, good_old, p_feat = tracker.track_features_sift(previous_frame, frame, feat, 1)
+        good_new, good_old, p_feat = tracker.track_features_sift(previous_frame, frame, feat)
 
-
-        frame = tracker.draw_frame(frame, pts_3d, p_feat, cameraMatrix, distCoeffs, model)
+        frame = tracker.draw_frame(frame, pts_3d, good_new, cameraMatrix, distCoeffs, model, h, w, p_feat)
 
         img = cv.add(frame, mask)
         imgSize = img.shape
 
         Images.append(img)
-
-        previous_frame = frame.copy()
+        if len(frame) > 0:
+            previous_frame = frame.copy()
+            #print(count)
 
         feat = good_new.reshape(-1, 1, 2)
 
