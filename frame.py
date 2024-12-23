07cb79f071c153191daf9a0c06738ef59df8199e
Olga-shimonis <simonisolga@gmail.com>
2024-12-23 13:41:29 +0300

    1

--- frame.py
+++ frame.py
@@ -1,7 +1,7 @@
 
 import cv2 as cv
 import numpy as np
-from registration import Model, register
+from registration_1 import Model, register
 from detection import Detector
 
 
@@ -10,20 +10,66 @@ from detection import Detector
 
 
 
-def detect_pose(p_feat, sift_3d): #height weight from model
+
+def detect_features(shot, model, h, w, img1):  #detecting features, matching features
+        MIN_MATCH_COUNT = 10
+        sift = cv.SIFT_create()
+        kp1, des1 = model.kp, model.des
+        #kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(shot, None)  #detect features
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(des1, des2, k=2)
+
+        good = []
+        for m, n in matches:
+            if m.distance < 0.6 * n.distance:
+                good.append(m)
+
+        if len(good) > MIN_MATCH_COUNT:
+            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+            matchesMask = mask.ravel().tolist()
+
+            features = []
+
+            ob = []
+            for i, m in enumerate(good):
+                if matchesMask[i]:
+                    features.append(dst_pts[i])
+                    ob.append(src_pts[i])
+
+            s_d = []
+            for i in ob:
+                x, y = i[0][0], i[0][1]
+                s_d.append([[x, y, 0]])
+            s_d = np.array(s_d)
+
+            features = np.array(features)
+
+        else:
+            print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+            matchesMask = None
+
+        return s_d, features  #3D features, 2D features
+
+
+def detect_pose(p_feat, sift_3d):
     if len(p_feat) > 3 and len(p_feat) == len(sift_3d):
         valid, rvec, tvec = cv.solvePnP(sift_3d, p_feat, cameraMatrix, distCoeffs)
         rvecs = cv.Rodrigues(rvec)[0]
         return valid, rvecs, tvec
-        #else:
-          #  return False
     return False
 
-def registration_transform_3d_corners(rvecs, tvec, cameraMatrix, distCoeffs): #height weight from model
-    pts = np.float32(model.object_corners_3d)
-    #pts = np.float32([[0, 0, 0], [0, h - 1, 0], [w - 1, h - 1, 0], [w - 1, 0, 0]]) #3d corners
-    t, _ = cv.projectPoints(pts, rvecs, tvec, cameraMatrix, distCoeffs)
 
+def registration_transform_3d_corners(rvecs, tvec, cameraMatrix, distCoeffs): #transform 3D coordinates of model corners to 2D
+    pts = np.float32(model.object_corners_3d) #3D coordinates of corners of model
+    t, _ = cv.projectPoints(pts, rvecs, tvec, cameraMatrix, distCoeffs) #transform to 2D coordinates using pose
     t = t.reshape(-1, 2)
     return t
 
@@ -63,61 +109,8 @@ class frame_by_registration:
         return model
 
 
-    def find_good_sift(self, shot, model, h, w, img1):
 
-        '''dst, features, s_d = detect_features(self, shot, model, h, w, img1)
-        #s_d = registration_find_3d_sifts(ob)'''
-        MIN_MATCH_COUNT = 10
-        sift = cv.SIFT_create()
-        kp1, des1 = model.kp, model.des
-        #kp1, des1 = sift.detectAndCompute(img1, None)
-        kp2, des2 = sift.detectAndCompute(shot, None)
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=50)
-        flann = cv.FlannBasedMatcher(index_params, search_params)
-
-        matches = flann.knnMatch(des1, des2, k=2)
-
-        good = []
-        for m, n in matches:
-            if m.distance < 0.6 * n.distance:
-                good.append(m)
-
-        if len(good) > MIN_MATCH_COUNT:
-            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-            matchesMask = mask.ravel().tolist()
-
-            #pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-            #dst = cv.perspectiveTransform(pts, M)
-            features = []
-
-            ob = []
-            for i, m in enumerate(good):
-                if matchesMask[i]:
-                    features.append(dst_pts[i])
-                    ob.append(src_pts[i])
-
-            s_d = []
-            for i in ob:
-                x, y = i[0][0], i[0][1]
-                s_d.append([x, y, 0])
-            s_d = np.array(s_d)
-
-            features = np.array(features)
-
-        else:
-            print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
-            matchesMask = None
-
-        return s_d, features
-
-
-
-    def track_features_sift(self, previous_frame, current_frame, feat):
+    def track_features_sift(self, previous_frame, current_frame, feat, sift_3d): #track features KLT
         frame_gray = cv.cvtColor(current_frame, cv.COLOR_BGR2GRAY)
         old_gray = cv.cvtColor(previous_frame, cv.COLOR_BGR2GRAY)
 
@@ -126,25 +119,29 @@ class frame_by_registration:
         good_new_feat = p_feat[st_feat == 1]
         good_old_feat = feat[st_feat == 1]
 
+        if len(good_new_feat) != len(sift_3d):   #matching 3D features with 2D features
+            new_3d = []
+            for i in range(len(st_feat)):
+                if st_feat[i] == 1:
+                    new_3d.append(sift_3d[i])
+            new_3d = np.array(new_3d)
+            print(new_3d.shape, good_new_feat.shape)
+        else:
+            new_3d = sift_3d
 
 
-
-
-        return good_new_feat, good_old_feat, p_feat
+        return good_new_feat, good_old_feat, p_feat, new_3d
 
 
 
     def draw_frame(self, frame, sift_3d, sift_2d, cameraMatrix, distCoeffs, model, h, w, p_feat):
-        #if detect_pose(sift_2d, sift_3d) != False:
-        print(len(p_feat), len(sift_3d))
         if len(p_feat) > 3 and len(p_feat) == len(sift_3d):
-            valid, rvec, tvec = cv.solvePnP(sift_3d, p_feat, cameraMatrix, distCoeffs)
-            rvecs = cv.Rodrigues(rvec)[0]
-            #valid, rvecs, tvec = detect_pose(sift_2d, sift_3d)
+            valid, rvecs, tvec = detect_pose(sift_2d, sift_3d)
             t = registration_transform_3d_corners(rvecs, tvec, cameraMatrix, distCoeffs)
             frame = cv.polylines(frame, [np.int32(t)], True, 255, 3, cv.LINE_AA)
 
         return frame
+
 
 
 
@@ -153,7 +150,7 @@ if __name__ == "__main__":
     img1 = cv.imread('reference.jpg', cv.IMREAD_GRAYSCALE)
     h, w = img1.shape
 
-    img_ = 'reference.jpg'   #load reference photo
+    img_ = 'reference.jpg'   #load reference photo for model
     camera_params = 'cam_params.npz'  #load camera parameters
 
     with np.load('cam_params.npz') as file:
@@ -171,7 +168,7 @@ if __name__ == "__main__":
         print("Failed to read the first frame.")
         exit()
 
-    pts_3d, feat = tracker.find_good_sift(previous_frame, model, h, w, img1)
+    pts_3d, feat = detect_features(previous_frame, model, h, w, img1)
     mask = np.zeros_like(previous_frame)
     Images = []
     n = 50
@@ -183,11 +180,9 @@ if __name__ == "__main__":
 
         if count % n == 0:
             ret, previous_frame = cap.read()
-            pts_3d, feat = tracker.find_good_sift(previous_frame, model, h, w, img1)
+            pts_3d, feat = detect_features(previous_frame, model, h, w, img1)
             mask = np.zeros_like(previous_frame)
-
-
-        good_new, good_old, p_feat = tracker.track_features_sift(previous_frame, frame, feat)
+        good_new, good_old, p_feat, pts_3d = tracker.track_features_sift(previous_frame, frame, feat, pts_3d)
 
         frame = tracker.draw_frame(frame, pts_3d, good_new, cameraMatrix, distCoeffs, model, h, w, p_feat)
 
