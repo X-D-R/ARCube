be18114fa017dc1c81f3b93609dfd3d1b54a39c1
Андрей <elkabor@mail.ru>
2025-01-15 15:39:03 +0300

    made parser but there is problem: No module named 'src'

--- src/main.py
+++ src/main.py
@@ -29,15 +29,17 @@ def parse_args_and_execute():
     register_parser.add_argument('--model_output', type=str, required=True,
                                  help="Path to save the model parameters")
 
-    # # Subcommand for detection
-    # detect_parser = subparsers.add_parser('detect', help="Detect features in an image or video")
-    #
-    # detect_parser.add_argument('--model_input', type=str, required=True, help="Path to the saved model file")
-    # detect_parser.add_argument('--camera_params', type=str, help="Path to camera parameters file (optional)")
-    # detect_parser.add_argument('--input_image', type=str, help="Path to input image for detection")
-    # detect_parser.add_argument('--input_video', type=str, help="Path to input video for detection")
-    # detect_parser.add_argument('--use_flann', action='store_true', help="Use FLANN-based matching")
-    # detect_parser.add_argument('--draw_match', action='store_true', help="Draw matches on the detected image/video")
+    # Subcommand for detection
+    detect_parser = subparsers.add_parser('detect', help="Detect features in an image or video")
+
+    detect_parser.add_argument('--model_input', type=str, required=True, help="Path to the saved model file")
+    detect_parser.add_argument('--camera_params', type=str, help="Path to camera parameters file (optional)")
+    detect_parser.add_argument('--input_image', type=str, help="Path to input image for detection")
+    detect_parser.add_argument('--input_video', type=str, help="Path to input video for detection")
+    detect_parser.add_argument('--video', action='store_true', help="True if you wand to detect video,"
+                                                                    "false if you want to detect photo")
+    detect_parser.add_argument('--output_image', type=str, help="Path to output image after detection")
+    detect_parser.add_argument('--output_video', type=str, help="Path to output video after detection")
 
     args = parser.parse_args()
 
@@ -55,17 +57,18 @@ def parse_args_and_execute():
             feature_method=args.feature_method,
             model_output=args.model_output
         )
-    # elif args.command == 'detect':
-    #     detect(
-    #         model_input=args.model_input,
-    #         camera_params=args.camera_params,
-    #         input_image=args.input_image,
-    #         input_video=args.input_video,
-    #         use_flann=args.use_flann if args.use_flann is not None else False,
-    #         draw_match=args.draw_match if args.draw_match is not None else False
-    #     )
-    # else:
-    #     print("Invalid command. Use 'register' or 'detect'.")
+        model = RectangleModel.load(args.model_output)
+        print(model)
+    elif args.command == 'detect':
+        detector = set_detector(args.model_input, args.camera_params)
+
+        if args.video:
+            track_frame(detector, args.input_video, args.output_video)
+        else:
+            img_points, src_pts, dst_pts = detector.detect_path(args.input_image)
+            draw_contours_of_rectangle(args.input_image, args.output_image, img_points)
+    else:
+        print("Invalid command. Use 'register' or 'detect'.")
 
 
 def register_to_model(object_corners_3d: np.ndarray, input_image: str, output_image: str, register_output: str, crop_method: str = 'corner', feature_method: str = 'SIFT') -> None:
@@ -119,45 +122,23 @@ if __name__ == "__main__":
     # example
     object_corners_3d = np.array([
         [0, 0, 0],  # Top-left
-        [0.13, 0, 0],  # Top-right
-        [0.13, 0.205, 0],  # Bottom-right
-        [0, 0.205, 0],  # Bottom-left
-
-    ], dtype="float32")
-    '''
-    register(
-        input_image="old_files/andrew photo video/reference messy.jpg",
-        output_image="OutputFiles/OutputImages/output_script_test.jpg",
-        object_corners_3d=object_corners_3d,
-        crop_method='corner', # or use crop_method='photo',
-        feature_method="ORB",
-        model_output="ModelParams/model_script_test.npz"
-    )
-    model = RectangleModel.load("ModelParams/model_script_test.npz")
-    print(model)
-    detector = Detector()
-    detector.set_model("CameraParams/CameraParams.npz")
-    detector.detect("")
-    '''
-
-    object_corners_3d = np.array([
-        [0, 0, 0],  # Top-left
         [0.14, 0, 0],  # Top-right
         [0.14, 0.21, 0],  # Bottom-right
         [0, 0.21, 0],  # Bottom-left
 
     ], dtype="float32") # example of object_corners_3d
-    # register_to_model(object_corners_3d, "../new_book_check/book_3.jpg", "../OutputFiles/OutputImages/output_script_test.jpg", "../ModelParams/model_script_test.npz", 'corner', "SIFT")
-    # set_model("../ModelParams/model_script_test.npz", "../ModelParams/model_test.npz")
+    register_to_model(object_corners_3d, "../new_book_check/book_3.jpg", "../OutputFiles/OutputImages/output_script_test.jpg", "../ModelParams/model_script_test.npz", 'corner', "SIFT")
+    set_model("../ModelParams/model_script_test.npz", "../ModelParams/model_test.npz")
 
     detector = set_detector("../ModelParams/model_test.npz", "../CameraParams/CameraParams.npz", True)
 
-    # img_points, src_pts, dst_pts = detector.detect_path("../examples/images/new_book_check.png")
-    # draw_contours_of_rectangle("../examples/images/new_book_check.png", "../OutputFiles/OutputImages/contours_drawn.png", img_points)
+    img_points, src_pts, dst_pts = detector.detect_path("../examples/images/new_book_check.png")
+    draw_contours_of_rectangle("../examples/images/new_book_check.png", "../OutputFiles/OutputImages/contours_drawn.png", img_points)
     track_frame(detector, "../new_book_check/new_book_video_main.mp4", "../OutputFiles/OutputVideos/new_book_video_main_result_new_color.mp4", 60, 30, (0, 0, 255))
 
     # # or
     # parse_args_and_execute()
     '''
-    python main.py register --input_image "../old_files/andrew photo video/reference messy.jpg" --output_image "../OutputFiles/OutputImages/output_script_test.jpg" --crop_method "corner" --points 0 0 0 13 0 0 13 20.5 0 0 20.5 0 --feature_method "ORB" --model_output "../ModelParams/model_script_test.npz"
+    python src\main.py register --input_image "../new_book_check/book_3.jpg" --output_image "../OutputFiles/OutputImages/output_script_test.jpg" --crop_method "corner" --points 0 0 0 0.14 0 0 0.14 0.21 0 0 0.21 0 --feature_method "SIFT" --model_output "../ModelParams/model_test.npz"
+    python src\main.py detect --model_input "../ModelParams/model_test.npz" --camera_params "../CameraParams/CameraParams.npz" --input_video "../new_book_check/new_book_video_main.mp4" --video --output_video "../OutputFiles/OutputVideos/new_book_video_main_result_new_color.mp4"
     '''
