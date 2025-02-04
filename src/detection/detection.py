de9a73559d34f78b4e4225a7075d8ad4b9413906
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2025-02-04 21:52:30 +0300

    added command line parameter to detect.py to visualize matches with the reference image

--- src/detection/detection.py
+++ src/detection/detection.py
@@ -148,7 +148,7 @@ class Detector:
             print("Not enough matches are found - {}/{}".format(len(good), self.MIN_MATCH_COUNT))
             img_points, inliers_original, inliers_frame, good, mask = None, None, None, None, None
 
-        return img_points, inliers_original, inliers_frame
+        return img_points, inliers_original, inliers_frame, kp2, good
 
     def detect(self, image: np.ndarray, coeff_lowes: int = 0.7) -> (np.ndarray, np.ndarray, np.ndarray):
         '''
