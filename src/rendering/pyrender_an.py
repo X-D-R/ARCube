64522029d4dbb203bec9547d1a9c492574d89189
Андрей <elkabor@mail.ru>
2025-02-11 14:10:57 +0300

    work (fixed mesh py in trimesh

--- src/rendering/pyrender_an.py
+++ src/rendering/pyrender_an.py
@@ -2,12 +2,10 @@ import numpy as np
 import trimesh
 import pyrender
 import matplotlib.pyplot as plt
-import os.path
 
-MAIN_DIR = os.path.dirname(os.path.abspath("register.py"))
 
-print(os.path.join(MAIN_DIR, "ExampleFiles", "3d_models", "bus.obj"))
-fuze_trimesh = trimesh.load(os.path.join(MAIN_DIR, "ExampleFiles", "3d_models", "bus.obj"))
+fuze_scene = trimesh.load('E:\\pycharm projects\\ARC\\ExampleFiles\\3d_models\\bus.obj')
+fuze_trimesh = fuze_scene.dump(concatenate=True)
 mesh = pyrender.Mesh.from_trimesh(fuze_trimesh)
 scene = pyrender.Scene()
 scene.add(mesh)
