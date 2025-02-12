cc6185c24b1f5a5ae2afdcfb33fa4db341320551
Андрей <elkabor@mail.ru>
2025-02-12 13:28:03 +0300

    idk ((

--- pyrender_an2.py
+++ pyrender_an2.py
@@ -2,11 +2,10 @@ import numpy as np
 import trimesh
 import pyrender
 import cv2 as cv
-import matplotlib.pyplot as plt
 from src.tracking.frame import detect_pose
 from src.registration.rectangle_model import RectangleModel
 
-fuze_trimesh = trimesh.load('E:\\pycharm projects\\ARC\\ExampleFiles\\3d_models\\box.obj')
+box_trimesh = trimesh.load('E:\\pycharm projects\\ARC\\ExampleFiles\\3d_models\\box.obj')
 
 scene = pyrender.Scene()
 
@@ -15,7 +14,7 @@ material = pyrender.MetallicRoughnessMaterial(
     metallicFactor=0.0,
     roughnessFactor=0.5
 )
-mesh = pyrender.Mesh.from_trimesh(fuze_trimesh, material=material)
+mesh = pyrender.Mesh.from_trimesh(box_trimesh, material=material)
 scene.add(mesh)
 
 model = RectangleModel.load('E:\\pycharm projects\\ARC\\ExampleFiles\\ModelParams\\model_test.npz')
@@ -38,18 +37,25 @@ camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0, aspectRatio=1.0)
 scene.add(camera, pose=camera_pose)
 # viewer = pyrender.Viewer(scene, use_raymond_lighting=True)
 
-image = cv.imread('E:\\pycharm projects\\ARC\\ExampleFiles\\examples\\images\\new_book_check.png')
+# image = cv.imread('E:\\pycharm projects\\ARC\\ExampleFiles\\examples\\images\\new_book_check.png')
+image = cv.imread('E:\\pycharm projects\\ARC\\ExampleFiles\\new_book_check\\book_3.jpg')
+
 h, w, channels = image.shape
 
 r = pyrender.OffscreenRenderer(viewport_width=w, viewport_height=h)
 color, depth = r.render(scene, flags=pyrender.RenderFlags.RGBA)
-alpha_mask = color[:, :, 3] / 255.0
-alpha_mask = np.repeat(alpha_mask[:, :, np.newaxis], 3, axis=2)
-color = color[:, :, :3]
 
-result = image.copy()
-result[alpha_mask > 0] = (color * alpha_mask + result * (1 - alpha_mask))[alpha_mask > 0]
+# 1 way
+# alpha_mask = color[:, :, 3] / 255.0
+# alpha_mask = np.repeat(alpha_mask[:, :, np.newaxis], 3, axis=2)
+# color = color[:, :, :3]
+# result = image.copy()
+# result[alpha_mask > 0] = (color * alpha_mask + result * (1 - alpha_mask))[alpha_mask > 0]
 
+# 2 way
+color = cv.cvtColor(color, cv.COLOR_RGB2BGR)
+alpha = 0.5
+result = cv.addWeighted(image, 1 - alpha, color, alpha, 0)
 
 max_height = 800
 h, w, channels = result.shape
