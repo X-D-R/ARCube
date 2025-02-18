93d2f8423006b456e95c5312e79ca0935755eae2
Андрей <elkabor@mail.ru>
2025-02-18 18:18:12 +0300

    faster

--- pyrender_an2.py
+++ pyrender_an2.py
@@ -10,24 +10,33 @@ class RenderPyrender:
     def __init__(self, w=640, h=480):
         self.scene = pyrender.Scene()
         self.renderer = pyrender.OffscreenRenderer(viewport_width=w, viewport_height=h)
-        self.mesh_node = None
+        self.mesh = None
+
     def load_obj(self, obj_path):
         """ Load 3D-model """
         mesh = trimesh.load_mesh(obj_path)
         mesh = pyrender.Mesh.from_trimesh(mesh)
-        self.mesh_node = pyrender.Node(mesh=mesh)
-        self.scene.add_node(self.mesh_node)
+        self.mesh = mesh
 
     def render(self, rvecs, tvecs, camera_matrix):
         """ Render 3D-object using pose """
-        if self.mesh_node is None:
+        if self.mesh is None:
             raise ValueError("3D-model is not loaded!")
 
         pose_obj = np.eye(4)
         pose_obj[:3, :3] = rvecs
         pose_obj[:3, 3] = tvecs.flatten()
-        self.scene.set_pose(self.mesh_node, pose_obj)
+        correction_rotation = np.array([
+            [1, 0, 0, 0],
+            [0, 0, -1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1]
+        ])
+        pose_obj = correction_rotation @ pose_obj
         print(pose_obj)
+
+        mesh_node = pyrender.Node(mesh=self.mesh, matrix=pose_obj)
+        self.scene.add_node(mesh_node)
 
         fx, fy = camera_matrix[0, 0], camera_matrix[1, 1]
         cx, cy = camera_matrix[0, 2], camera_matrix[1, 2]
@@ -35,7 +44,7 @@ class RenderPyrender:
         # cam = pyrender.PerspectiveCamera(yfov=np.pi / 3.0, aspectRatio=1.0)
 
         pose_cam = np.eye(4)
-        pose_cam[:3, 3] = np.array([0, 0, 0.4])
+        pose_cam[:3, 3] = np.array([0, 0, 0])
         print(pose_cam)
 
         cam_node = pyrender.Node(camera=cam, matrix=pose_cam)
@@ -49,10 +58,12 @@ class RenderPyrender:
         return color
 
 print('setting detector')
+
 cam_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\CameraParams\\CameraParams.npz'
-model_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\ModelParams\\model_test.npz'
+model_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\ModelParams\\model_script_test.npz'
 frame = cv.imread('E:\\pycharm projects\\ARC\\ExampleFiles\\new_book_check\\book_3.jpg')
-obj_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\3d_models\\box_2.obj'
+# frame = cv.imread('E:\\pycharm projects\\ARC\\ExampleFiles\\examples\\images\\new_book_check.png')
+obj_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\3d_models\\box.obj'
 
 detector = set_detector(model_path, cam_path)
 camera_matrix = detector.camera_params['mtx']
@@ -62,18 +73,20 @@ print('loading obj')
 rend = RenderPyrender(frame.shape[1], frame.shape[0])
 rend.load_obj(obj_path)
 
-
-img_points = 1
+print('detecting pose')
+# img_points = 1
+img_points, inliers_original, inliers_frame, kp, good, homography, mask = detector.detect(frame)
 
 if img_points is not None:
-    print('detecting pose')
-    # img_points, inliers_original, inliers_frame, kp, good, homography, mask = detector.detect(frame)
-    # valid, rvecs, tvec = detect_pose(inliers_frame, inliers_original, camera_matrix, dist_coeffs)
-    valid = True
-    rvecs = np.array([[ 0.9989321,   0.00911396,  0.04529451],
-                    [-0.0081956,   0.9997579,  -0.02041991],
-                    [-0.04546965,  0.02002689,  0.99876495]])
-    tvec = np.array([ -0.07633527,   -0.10293312,  0.18360005])
+
+    valid, rvecs, tvec = detect_pose(inliers_frame, inliers_original, camera_matrix, dist_coeffs)
+    frame = cv.polylines(frame, [np.int32(img_points)], True, 255, 3, cv.LINE_AA)
+
+    # valid = True
+    # rvecs = np.array([[ 0.99928102,  0.01074149,  0.03636013],
+    #                 [-0.00892128,  0.99871648, -0.0498579],
+    #                 [-0.03684901,  0.04949768,  0.99809425]])
+    # tvec = np.array([ -0.07684176,   -0.10242596,  0.176132])
 
     if valid:
         print('rendering')
