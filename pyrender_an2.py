477b4d3ed669baf97baf2337f9ee52aea3fe77c4
Андрей <elkabor@mail.ru>
2025-02-18 15:41:12 +0300

    faster

--- pyrender_an2.py
+++ pyrender_an2.py
@@ -35,37 +35,48 @@ class RenderPyrender:
         # cam = pyrender.PerspectiveCamera(yfov=np.pi / 3.0, aspectRatio=1.0)
 
         pose_cam = np.eye(4)
-        pose_cam[:3, 3] = np.array([0, 0, 0.21])
+        pose_cam[:3, 3] = np.array([0, 0, 0.4])
         print(pose_cam)
 
         cam_node = pyrender.Node(camera=cam, matrix=pose_cam)
         self.scene.add_node(cam_node)
 
-        viewer = pyrender.Viewer(self.scene, use_raymond_lighting=True)
+        # pyrender.Viewer(self.scene, use_raymond_lighting=True)
 
         color, _ = self.renderer.render(self.scene)
         self.scene.remove_node(cam_node)
 
         return color
 
-
+print('setting detector')
 cam_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\CameraParams\\CameraParams.npz'
 model_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\ModelParams\\model_test.npz'
 frame = cv.imread('E:\\pycharm projects\\ARC\\ExampleFiles\\new_book_check\\book_3.jpg')
-obj_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\3d_models\\box.obj'
+obj_path = 'E:\\pycharm projects\\ARC\\ExampleFiles\\3d_models\\box_2.obj'
 
 detector = set_detector(model_path, cam_path)
 camera_matrix = detector.camera_params['mtx']
 dist_coeffs = detector.camera_params['dist']
 
+print('loading obj')
 rend = RenderPyrender(frame.shape[1], frame.shape[0])
 rend.load_obj(obj_path)
 
-img_points, inliers_original, inliers_frame, kp, good, homography, mask = detector.detect(frame)
+
+img_points = 1
 
 if img_points is not None:
-    valid, rvecs, tvec = detect_pose(inliers_frame, inliers_original, camera_matrix, dist_coeffs)
+    print('detecting pose')
+    # img_points, inliers_original, inliers_frame, kp, good, homography, mask = detector.detect(frame)
+    # valid, rvecs, tvec = detect_pose(inliers_frame, inliers_original, camera_matrix, dist_coeffs)
+    valid = True
+    rvecs = np.array([[ 0.9989321,   0.00911396,  0.04529451],
+                    [-0.0081956,   0.9997579,  -0.02041991],
+                    [-0.04546965,  0.02002689,  0.99876495]])
+    tvec = np.array([ -0.07633527,   -0.10293312,  0.18360005])
+
     if valid:
+        print('rendering')
         rendered = rend.render(rvecs, tvec, camera_matrix)
         rendered = cv.resize(rendered, (frame.shape[1], frame.shape[0]))
         rendered = cv.cvtColor(rendered, cv.COLOR_RGB2BGR)
